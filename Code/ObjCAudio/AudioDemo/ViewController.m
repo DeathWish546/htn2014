28c42fe3bd477428ca315b0f37f9c3472ef8f0a4
Yang Chen <Yang@Yangs-MacBook-Pro.local>
2014-12-19 15:46:26 -0500

    all

--- Code/ObjCAudio/AudioDemo/ViewController.m
+++ Code/ObjCAudio/AudioDemo/ViewController.m
@@ -28,23 +28,23 @@
     [playButton setEnabled:NO];
     
     // Set the audio file
-    NSArray *pathComponents = [NSArray arrayWithObjects:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],@"Bass.m4a",nil];
+    NSArray *pathComponents = [NSArray arrayWithObjects:[NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) lastObject],@"Beatboxing.m4a",nil];
 
     NSURL *recordedURL = [NSURL fileURLWithPathComponents:pathComponents];
     NSURL *backgroundURL = [NSURL fileURLWithPath:[[NSBundle mainBundle] pathForResource:@"MorrisonTrioSample-AirbyJSBach" ofType:@"m4a"]];
     
-    NSString *stringURL = @"http://omgwow.cloudapp.net:9292/files/Beatboxing.m4a";
-    NSURL  *url = [NSURL URLWithString:stringURL];
-    NSData *urlData = [NSData dataWithContentsOfURL:url];
-    NSString *filePath;
-    if ( urlData )
-    {
-        NSArray       *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-        NSString  *documentsDirectory = [paths objectAtIndex:0];
-        
-        filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory,@"Beatboxing.m4a"];
-        [urlData writeToFile:filePath atomically:YES];
-    }
+//    NSString *stringURL = @"http://omgwow.cloudapp.net:9292/files/Beatboxing.m4a";
+//    NSURL  *url = [NSURL URLWithString:stringURL];
+//    NSData *urlData = [NSData dataWithContentsOfURL:url];
+//    NSString *filePath;
+//    if ( urlData )
+//    {
+//        NSArray       *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
+//        NSString  *documentsDirectory = [paths objectAtIndex:0];
+//        
+//        filePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory,@"Beatboxing.m4a"];
+//        [urlData writeToFile:filePath atomically:YES];
+//    }
 
     // Setup audio session
     AVAudioSession *session = [AVAudioSession sharedInstance];
@@ -66,9 +66,9 @@
     player_background = [[AVAudioPlayer alloc] initWithContentsOfURL:backgroundURL error:nil];
     //[soundsArray addObject:player_background];
     
-    NSURL *backgroundURL2 = [NSURL fileURLWithPath:filePath];
-    player_background2 = [[AVAudioPlayer alloc] initWithContentsOfURL:backgroundURL2 error:nil];
-    [soundsArray addObject:player_background2];
+//    NSURL *backgroundURL2 = [NSURL fileURLWithPath:filePath];
+//    player_background2 = [[AVAudioPlayer alloc] initWithContentsOfURL:backgroundURL2 error:nil];
+//    [soundsArray addObject:player_background2];
     
     for (AVAudioPlayer *a in soundsArray) [a prepareToPlay];
 }
